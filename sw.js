0f693bbd8c19a07560d7bcf2b290f64c99239119
ankitmallick162002-art <ankitmallick162002@gmail.com>
2026-09-09 21:54:32 +0530

    Update service worker to version 3 with enhancements

--- sw.js
+++ sw.js
@@ -1,5 +1,68 @@
-const CACHE='sahay-v1';
-const ASSETS=['./','./index.html','./manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html')))));
+const CACHE = 'sahay-v3';
+const PRECACHE = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(PRECACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(k => k !== CACHE)
+            .map(k => caches.delete(k))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+
+  if (request.method !== 'GET') return;
+
+  if (
+    request.mode === 'navigate' ||
+    request.destination === 'document'
+  ) {
+    event.respondWith(
+      fetch(request)
+        .then(response => {
+          const copy = response.clone();
+
+          caches.open(CACHE)
+            .then(cache => cache.put('./index.html', copy))
+            .catch(() => {});
+
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request)
+      .then(cached => {
+        if (cached) return cached;
+
+        return fetch(request).then(response => {
+          const copy = response.clone();
+
+          caches.open(CACHE)
+            .then(cache => cache.put(request, copy))
+            .catch(() => {});
+
+          return response;
+        });
+      })
+  );
+});
